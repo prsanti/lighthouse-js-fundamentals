10479abb7dea8e843e9c17e592c22928445f2e7a
Paulo Santiago <p.santiago796@gmail.com>
2020-10-02 20:07:19 +0000

    X marks the perfect shot assignment, with indentation for the code eval.

---
+++ x-marks.js
@@ -0,0 +1,23 @@
+const finalPosition = function (moves) {
+  let position = [0,0];
+  for (let move of moves) {
+    switch (move) {
+    case "north":
+      position[1] += 1;
+      break;
+    case "south":
+      position[1] -= 1;
+      break;
+    case "east":
+      position[0] += 1;
+      break;
+    case "west":
+      position[0] -= 1;
+      break;
+    }
+  }
+  return position;
+}
+// Testing array of moves and printing it to the console.
+//const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+//console.log(finalPosition(moves));
