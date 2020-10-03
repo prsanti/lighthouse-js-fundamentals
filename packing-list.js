c60c9d5449dc9304aa56e1f15095468b541d5438
Paulo Santiago <p.santiago796@gmail.com>
2020-10-03 21:05:43 +0000

    example code

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+// example code
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
